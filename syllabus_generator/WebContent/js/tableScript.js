be47546ad3215cf5db04c6a44752485c15a65a59
Adrian Obleton <obletona@uga.edu>
2016-11-13 19:20:28 -0500

    Fixed error in js caused by string being on different line.

--- syllabus_generator/WebContent/js/tableScript.js
+++ syllabus_generator/WebContent/js/tableScript.js
@@ -5,12 +5,7 @@ $(document).ready(function(){
     $("#submit").click(function(event){
         event.preventDefault();
 
-        var newRow = $('<tr>
-      <th scope="col"> <input type="number" name="number"></th>
-      <th scope="col"><input type="text" name="textfield23" id="textfield23"></th>
-      <th scope="col"><input type="text" name="textfield24" id="textfield24"></th>
-      <th scope="col"><input type="text" name="textfield25" id="textfield25"></th>
-    </tr>');
+        var newRow = $('<tr><th scope="col"> <input type="number" name="number"></th><th scope="col"><input type="text" name="textfield23" id="textfield23"></th><th scope="col"><input type="text" name="textfield24" id="textfield24"></th><th scope="col"><input type="text" name="textfield25" id="textfield25"></th></tr>');
             counter++;
         jQuery('#dateTable').append(newRow);
 
