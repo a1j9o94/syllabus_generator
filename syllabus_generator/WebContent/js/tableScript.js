92df8afd3744208d3e4b77ec3cfa4dcdd461535f
a1j9o94 <obletonadrian@gmail.com>
2016-11-14 15:59:38 -0500

    Modified index to match the database requirements

--- syllabus_generator/WebContent/js/tableScript.js
+++ syllabus_generator/WebContent/js/tableScript.js
@@ -1,25 +1,39 @@
 $(document).ready(function(){
-    var counter = 1;
+    var dateCounter = 1;
+    var gradeCounter = 1;
+    var eventsCounter = 1;
+    var bookCounter = 1;
     $("#dateTableAddRow").click(function(event){
+    	dateCounter++;
         event.preventDefault();
 
-        var newRow = $('<tr><th scope="col"><input type="date"></th><th><input type="text"></th><th><input type="text"></th></tr>');
-            counter++;
-        jQuery('#dateTable').append(newRow);
+        var newRow = $('<tr><td><input type="date" name="date-' + dateCounter + '"></td><td><input type="text" name="topic-' + dateCounter + '"></td><td><input type="text" name="assignments-' + dateCounter + '"></td></tr>');
+        $('#dateTable').append(newRow);
 
     });
-});
-
-
-
-$(document).ready(function(){
-    var counter = 1;
+    
     $("#gradeDistAddRow").click(function(event){
+    	gradeCounter++;
         event.preventDefault();
 
-        var newRow = $(' <tr><td align="center"><input name="textfield3" type="text" id="textfield3" size="50"></td><td align="center"><input name="textfield12" type="text" id="textfield13" size="50"></td></tr>');
-            counter++;
-        jQuery('#gradeDistTable').append(newRow);
+        var newRow = $(' <tr><td><input name="courseAspect-' + gradeCounter + '" type="text"></td><td><input name="gradeWeight-' + gradeCounter + '" type="text"></td></tr>');
+        $('#gradeDistTable').append(newRow);
 
+    });
+    $("#eventsTableAddRow").click(function(event){
+    	eventsCounter++;
+        event.preventDefault();
+
+        var newRow = $(' <tr><td><input name="eventDate-' + eventsCounter + '" type="text"></td><td><input name="eventName-' + eventsCounter + '" type="text"></td><td><input type="text" name="eventLocation-' + eventsCounter + '"></td></tr>');
+        $('#eventsTable').append(newRow);
+
+    });
+    
+    $("#addBook").click(function(event){
+    	bookCounter++;
+    	event.preventDefault();
+    	
+    	var newRow = $('<td> <input type="text" name="bookName-' + bookCounter + '"></td><td><input type="checkbox" name="bookRequired-' + bookCounter + '"></td>');
+    	$("#bookTable").append(newRow);
     });
 });
