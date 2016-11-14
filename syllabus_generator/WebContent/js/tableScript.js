cbd8d1a4ffff20e64516bd55e29700498a395c18
Seabass <Seabass@s172-21-240-h16.paws.uga.edu>
2016-11-13 20:04:34 -0500

    changed formatting that was ruining functionality

--- syllabus_generator/WebContent/js/tableScript.js
+++ syllabus_generator/WebContent/js/tableScript.js
@@ -1,16 +1,9 @@
-
-
 $(document).ready(function(){
     var counter = 1;
     $("#submit").click(function(event){
         event.preventDefault();
 
-        var newRow = $('<tr>
-							<th scope="col">
-							<input type="date"></th>
-							<th><input type="text"></th>
-							<th><input type="text"></th>
-					    </tr>');
+        var newRow = $('<tr><th scope="col"><input type="date"></th><th><input type="text"></th><th><input type="text"></th></tr>');
             counter++;
         jQuery('#dateTable').append(newRow);
 
