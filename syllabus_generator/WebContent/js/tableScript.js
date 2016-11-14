9139712dd385c1e102ae514f8e3edf4c08a4b266
Seabass <Seabass@s172-21-240-h16.paws.uga.edu>
2016-11-13 19:16:29 -0500

    hello 2.0

---
+++ syllabus_generator/WebContent/js/tableScript.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){
+    var counter = 1;
+    $("#submit").click(function(event){
+        event.preventDefault();
+
+        var newRow = $('<tr>
+      <th scope="col"> <input type="number" name="number"></th>
+      <th scope="col"><input type="text" name="textfield23" id="textfield23"></th>
+      <th scope="col"><input type="text" name="textfield24" id="textfield24"></th>
+      <th scope="col"><input type="text" name="textfield25" id="textfield25"></th>
+    </tr>');
+            counter++;
+        jQuery('#dateTable').append(newRow);
+
+    });
+});
