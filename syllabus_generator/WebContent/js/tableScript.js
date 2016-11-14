3479e7d518fbd216a2c5871f4335b9774402a99b
Seabass <Seabass@s172-21-240-h16.paws.uga.edu>
2016-11-13 20:29:53 -0500

    Changed grade table to grade weight distribution table, script included

--- syllabus_generator/WebContent/js/tableScript.js
+++ syllabus_generator/WebContent/js/tableScript.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
     var counter = 1;
-    $("#submit").click(function(event){
+    $("#dateTableAddRow").click(function(event){
         event.preventDefault();
 
         var newRow = $('<tr><th scope="col"><input type="date"></th><th><input type="text"></th><th><input type="text"></th></tr>');
             counter++;
         jQuery('#dateTable').append(newRow);
+
+    });
+});
+
+
+
+$(document).ready(function(){
+    var counter = 1;
+    $("#gradeDistAddRow").click(function(event){
+        event.preventDefault();
+
+        var newRow = $(' <tr><td align="center"><input name="textfield3" type="text" id="textfield3" size="50"></td><td align="center"><input name="textfield12" type="text" id="textfield13" size="50"></td></tr>');
+            counter++;
+        jQuery('#gradeDistTable').append(newRow);
 
     });
 });
